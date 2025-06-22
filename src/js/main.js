76c2d276e18872bb23a45308ebd86195f0ff0dd0
Victor <victor3790@gmail.com>
2025-06-22 17:34:44 -0600

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// Import all of Bootstrap’s JS
+//import * as bootstrap from 'bootstrap';
+
+// Import our custom CSS
+import '../scss/styles.scss'
+import '../scss/main.scss'
+
+
+// Import the HTML file
+import '../index.html';
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/main.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html', filename: 'index.html' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader',
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                // Optional: Silence Sass deprecation warnings. See note below.
+                silenceDeprecations: [
+                  'mixed-decls',
+                  'color-functions',
+                  'global-builtin',
+                  'import'
+                ],
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+     },
+    ]
+  },
+  target: 'web',
+}
