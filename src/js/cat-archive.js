e9313ea5129b8ac55080f842f6f3e9082b4dc7d6
Victor <victor3790@gmail.com>
2026-06-18 17:15:11 -0600

    top spacing in categories page fixed

--- src/js/cat-archive.js
+++ src/js/cat-archive.js
@@ -1,6 +1,7 @@
 // Import our custom CSS
 import '../scss/bootstrap.scss'
 import '../scss/styles.scss'
+import '../scss/breadcrumbs.scss'
 import '../scss/archive.scss'
 import '../scss/sidebar.scss'
 
